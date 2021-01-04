3a35959677f54defc1d167901d2c7440caa7d831
Woojo Kim <kwoojo0127@gmail.com>
2021-01-04 20:30:18 +0900

    Add tic-tac-toe initial codes

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div class="board">
+      <div v-for="(row, rowIndex) in boards" :key="rowIndex">
+        <sqaure
+          v-for="(col, colIndex) in row"
+          :key="rowIndex * 10 + colIndex"
+          @square-click="handleClick(rowIndex, colIndex)"
+          :mark="col"
+        />
+      </div>
+    </div>
+    <div>Player: {{ player }}</div>
+  </div>
+</template>
+
+<script>
+import Sqaure from './components/Sqaure.vue';
+
+const checkWinner = (boards) => {
+  
+}
+
+export default {
+  name: 'App',
+  components: {
+    Sqaure,
+  },
+  data() {
+    return {
+      player: 'O',
+      boards: [Array(3), Array(3), Array(3)],
+    };
+  },
+  methods: {
+    handleClick(row, col) {
+      if (this.boards[row][col]) {
+        return;
+      }
+
+      this.boards[row][col] = this.player;
+      this.$forceUpdate();
+      this.player = this.player === 'O' ? 'X' : 'O';
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+}
+
+.board {
+  display: flex;
+}
+</style>
---
+++ tic-tac-toe/src/components/Sqaure.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="square" @click="$emit('square-click')">{{ mark }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: {
+    mark: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.square {
+  border: 1px solid #ddd;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+}
+</style>
